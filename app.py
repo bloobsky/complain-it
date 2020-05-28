e9e1936ed84182dae3d8d08db084e8fc8a5baaa0
bloobsky <bloobsky@gmail.com>
2020-05-28 18:04:13 +0000

    Changed directory structure

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 from os import path
-from flask import Flask
+from flask import Flask, render_template, url_for, redirect, request
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
@@ -17,8 +17,8 @@ mongo = PyMongo(app)
 
 
 @app.route('/')
-def default():
-    return 'Hello World'
+def index():
+    return render_template("index.html")
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'), port=int(os.environ.get('PORT')), debug=True)

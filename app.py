4a47d20fc65a8e1593dd2e408773de06d1683274
bloobsky <bloobsky@gmail.com>
2020-05-29 11:40:06 +0000

    Added ability of dynamic Title Pages in App.py

--- app.py
+++ app.py
@@ -19,35 +19,35 @@ mongo = PyMongo(app)
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template("index.html")
+    return render_template("index.html", title="Home")
 
 @app.route('/search_for')
 def search_for():
-    return render_template('search_for.html')
+    return render_template('search_for.html', title="Search for complained jobs")
 
 @app.route('/complain')
 def complain():
-    return render_template('complain.html')
+    return render_template('complain.html', title="Complain a job")
 
 @app.route('/works')
 def works():
-    return render_template('works.html')
+    return render_template('works.html', title="How it works")
 
 @app.route('/faq')
 def faq():
-    return render_template('faq.html')
+    return render_template('faq.html', title="Frequently Asked Questions")
 
 @app.route('/privacy')
 def privacy():
-    return render_template('privacy.html')
+    return render_template('privacy.html', title="Privacy Policy")
 
 @app.route('/contact')
 def contact():
-    return render_template('contact.html')
+    return render_template('contact.html', title="Contact Us")
 
 @app.route('/directions')
 def directions():
-    return render_template('directions.html')
+    return render_template('directions.html', title="Directions")
 
 
 

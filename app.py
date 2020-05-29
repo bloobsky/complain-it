113d4827412b76392619ac79680fada9bee9d6e5
bloobsky <bloobsky@gmail.com>
2020-05-29 21:18:10 +0000

    Fixed Issue with jQuery and Flask not working properly together when using Contact Form

--- app.py
+++ app.py
@@ -1,13 +1,26 @@
 import os
 from os import path
 from flask import Flask, render_template, url_for, redirect, request
+from flask_mail import Mail, Message
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+import smtplib
 
 if path.exists("env.py"):
   import env 
 
 app = Flask(__name__)
+
+app.config['MAIL_SERVER'] = 'smtp.office365.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_SSL'] = False
+app.config['MAIL_USERNAME'] = 'mattie@jakusz.org'
+app.config['MAIL_PASSWORD'] = '@Qu4n74sst1c'
+app.config['MAIL_DEBUG'] = True
+app.config["MAIL_DEFAULT_SENDER"] = 'bloobsky@gmail.com'
+mail = Mail(app)
+
 
 app.config["MONGO_DBNAME"] = 'complain-it'
 app.config["MONGO_URI"] = os.getenv("MONGOSRC")
@@ -44,6 +57,7 @@ def privacy():
 @app.route('/contact', methods=['GET', 'POST'])
 def contact():
     return render_template('contact.html', title="Contact Us")
+    
 
 @app.route('/directions')
 def directions():
--- static/js/sendMail.js
+++ static/js/sendMail.js
@@ -1,8 +1,8 @@
-function sendMail(contactForm) {
+/*function sendMail(contactForm) {
     emailjs.send("gmail", "project", {
        "from_name": contactForm.firstname.value,
        "from_lastname": contactForm.lastname.value, 
-       "from_email": contactForm.email.value,
+    "from_email": contactForm.email.value,
        "tel_number": contactForm.tel.value,
        "message": contactForm.message.value
     })
@@ -15,3 +15,24 @@ function sendMail(contactForm) {
         })
     return false;
 }
+*/
+
+var myform = $("form#myform");
+myform.submit(function(event){
+	event.preventDefault();
+
+  // Change to your service ID, or keep using the default service
+  var service_id = "gmail";
+  var template_id = "project";
+
+  myform.find("button").text("Sending...");
+  emailjs.sendForm(service_id,template_id,myform[0])
+  	.then(function(){ 
+    	alert("We received your message! We try to respond in next 24hours. Stay patient!");
+       myform.find("button").text("Send");
+    }, function(err) {
+       alert("Send email failed!\r\n Response:\n " + JSON.stringify(err));
+       myform.find("button").text("Send");
+    });
+  return false;
+});

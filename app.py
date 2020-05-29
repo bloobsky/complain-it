79483e5993199f9c5134a15ff857cc891c950549
bloobsky <bloobsky@gmail.com>
2020-05-29 21:45:50 +0000

    Removed Unneccessary Code from App.py

--- app.py
+++ app.py
@@ -1,33 +1,17 @@
 import os
 from os import path
 from flask import Flask, render_template, url_for, redirect, request
-from flask_mail import Mail, Message
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
-import smtplib
-
 if path.exists("env.py"):
   import env 
 
 app = Flask(__name__)
 
-app.config['MAIL_SERVER'] = 'smtp.office365.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USE_SSL'] = False
-app.config['MAIL_USERNAME'] = 'mattie@jakusz.org'
-app.config['MAIL_PASSWORD'] = '@Qu4n74sst1c'
-app.config['MAIL_DEBUG'] = True
-app.config["MAIL_DEFAULT_SENDER"] = 'bloobsky@gmail.com'
-mail = Mail(app)
-
-
 app.config["MONGO_DBNAME"] = 'complain-it'
 app.config["MONGO_URI"] = os.getenv("MONGOSRC")
 
 mongo = PyMongo(app)
-
-
 
 @app.route('/')
 @app.route('/index')
